30201a430b6d71b132454292765a8688731358d1
saud_c-c <swda8731@gmail.com>
2024-10-07 13:56:43 +0300

    first commit

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+ 
+    <title><?php echo $page_title; ?></title>
+ 
+    <!-- Latest compiled and minified Bootstrap CSS -->
+  
+    <!-- our custom CSS -->
+    <link rel="stylesheet" href="custom.css" />
+  
+</head>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<body>
+ 
+    <!-- container -->
+    <div class="container">
+ 
+        <?php
+        // show page header
+        echo "<div class='page-header'>
+                <h1>{$page_title}</h1>
+            </div>";
+        ?>
